5efac1b21aa38606ae56c1184975c4f7c62a6a42
SM03594 <santiago.margarit@alu.inspt.utn.edu.ar>
2025-10-10 11:52:34 -0300

    plantilla proyecto

---
+++ src/test/java/com/cyberkiosco/cyberkiosco_springboot/CyberkioscoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyberkiosco.cyberkiosco_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberkioscoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
