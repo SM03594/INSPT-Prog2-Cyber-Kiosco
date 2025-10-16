bb22229e213b867b5f4001e388756b3daa37a7af
SM03594 <santiago.margarit@alu.inspt.utn.edu.ar>
2025-10-16 12:45:01 -0300

    BDD H2 para tests implementada con scipts sql de iniciacion

---
+++ src/test/resources/schema.sql
@@ -0,0 +1,86 @@
+
+
+CREATE TABLE `categoria` (
+	`id_categoria` INTEGER NOT NULL AUTO_INCREMENT,
+	`nombre` VARCHAR(50) NOT NULL UNIQUE,
+	PRIMARY KEY(`id_categoria`)
+);
+
+
+CREATE TABLE `marca` (
+	`id_marca` INTEGER NOT NULL AUTO_INCREMENT,
+	`nombre` VARCHAR(255) NOT NULL UNIQUE,
+	PRIMARY KEY(`id_marca`)
+);
+
+
+CREATE TABLE `producto` (
+	`id_producto` INTEGER NOT NULL AUTO_INCREMENT,
+	`nombre` VARCHAR(50) NOT NULL,
+	`stock` INTEGER NOT NULL,
+	`precio` DOUBLE NOT NULL,
+	`imagen` VARCHAR(255) NOT NULL,
+	`descripcion` VARCHAR(255),     --modificacion para que acepte valores nulos
+	`id_categoria` INTEGER NOT NULL,
+	`id_marca` INTEGER NOT NULL,
+	PRIMARY KEY(`id_producto`)
+);
+
+
+CREATE TABLE `carrito` (
+	`id_carrito` INTEGER NOT NULL AUTO_INCREMENT,
+	`precio_total` DOUBLE NOT NULL,
+	`fecha_compra` DATETIME NOT NULL,
+	`id_usuario` INTEGER NOT NULL,
+	PRIMARY KEY(`id_carrito`)
+);
+
+
+CREATE TABLE `carrito_producto` (
+	`id_carrito` INTEGER NOT NULL,
+	`id_producto` INTEGER NOT NULL,
+	`cantidad_producto` INTEGER NOT NULL,
+	`precio_producto` DOUBLE NOT NULL,
+	PRIMARY KEY(`id_carrito`, `id_producto`)
+);
+
+
+CREATE TABLE `usuario` (
+	`id_usuario` INTEGER NOT NULL AUTO_INCREMENT,
+	`nombre` VARCHAR(255) NOT NULL UNIQUE,
+	`apellido` VARCHAR(255) NOT NULL,
+	`mail` VARCHAR(255) NOT NULL,
+	`password` VARCHAR(255) NOT NULL,
+	`balance` DOUBLE NOT NULL DEFAULT 0,
+	`rol` ENUM('ADMIN', 'CLIENTE') NOT NULL,
+	PRIMARY KEY(`id_usuario`)
+);
+
+
+CREATE TABLE `perfil` (
+	`id_perfil` INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+	`nombre_perfil` VARCHAR(255) NOT NULL,
+	`foto` VARCHAR(255) NOT NULL,
+	`id_usuario` INTEGER NOT NULL UNIQUE,
+	PRIMARY KEY(`id_perfil`)
+);
+
+
+ALTER TABLE `producto`
+ADD FOREIGN KEY(`id_categoria`) REFERENCES `categoria`(`id_categoria`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE `producto`
+ADD FOREIGN KEY(`id_marca`) REFERENCES `marca`(`id_marca`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE `carrito_producto`
+ADD FOREIGN KEY(`id_carrito`) REFERENCES `carrito`(`id_carrito`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE `carrito_producto`
+ADD FOREIGN KEY(`id_producto`) REFERENCES `producto`(`id_producto`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE `carrito`
+ADD FOREIGN KEY(`id_usuario`) REFERENCES `usuario`(`id_usuario`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE `perfil`
+ADD FOREIGN KEY(`id_usuario`) REFERENCES `usuario`(`id_usuario`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
